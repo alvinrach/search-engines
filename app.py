c19f5198ba878a771af7c384f797a14a76766a0b
alvinrach <alvinracht@gmail.com>
2024-08-07 13:03:13 +0700

    Update dashboard

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 import streamlit as st
+from modules import bm25
+import json
 
-st.write('First Test')
+st.title('Similar Account')
+
+st.warning(
+    """
+    This app is developed to find similarty between TP standard account name and similar account names using bm25 algorithm.
+
+    The app is purely prototype, still in development cycle, and need to clarify to client whether this is what the client wants or not.
+    """
+)
+
+with open("assets.json", "r") as read_content: 
+    assets = json.load(read_content)
+
+option = st.selectbox(
+    "Select TP Standard Account Name",
+    assets,
+    index=None,
+    placeholder="Select one...",
+)
+
+st.markdown('# Top 2 Most Similar Items:')
+
+try:
+    results = bm25(query_item=option)
+    for j,i in enumerate(results):
+        st.write(f'{j+1}. {i}')
+except:
+    st.write('Select the account name first')
